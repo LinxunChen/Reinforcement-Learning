f46db0cd9122053c0339e1382d335532adaaa9da
LinxunChen <clx1991@mail.ustc.edu.cn>
2020-12-05 17:24:03 +0800

    add dynamic loss and reward evaluation

--- RL_brain.py
+++ RL_brain.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-from tensorflow.python import keras
 from tensorflow.python.keras import Input, Model
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.optimizer_v2.adam import Adam
-import matplotlib.pyplot as plt
 
 
 class DQN:
@@ -39,7 +37,8 @@ class DQN:
         self.learn_step_counter = 0
         self.memory = np.zeros((self.memory_size, n_features * 2 + 2))
         self._build_net()
-        self.cost_his = []
+        # self.each_epoch_cost_his = []
+        self.each_step_cost_his = []
 
     def target_net_replace_op(self):
         w1 = self.model_eval.get_weights()
@@ -63,11 +62,6 @@ class DQN:
         batch_memory = self.memory[np.random.randint(0, min(self.memory_counter, self.memory_size), self.batch_size), :]
 
         # 构造q_target
-        # q_next, q_eval 包含所有 action 的值，而我们需要的只是已经选择好的 action 的值, 其他的并不需要。
-        # 所以我们将其他的 action 值全变成 0, 将用到的 action 误差值 反向传递回去, 作为更新凭据。
-        # 将 q_eval 全部赋值给 q_target, 这时 q_target-q_eval 全为 0,
-        # 不过 我们再根据 batch_memory 当中的 action 这个 column 来给 q_target 中的对应的 memory-action 位置来修改赋值.
-        # 使新的赋值为 reward + gamma * maxQ(s_), 这样 q_target-q_eval 就可以变成我们所需的样子.
         q_next, q_eval = self.model_target.predict(batch_memory[:, -self.n_features:]), self.model_eval.predict(
             batch_memory[:, :self.n_features])
         q_target = q_eval.copy()
@@ -76,8 +70,10 @@ class DQN:
         reward = batch_memory[:, self.n_features + 1]
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
 
-        history = self.model_eval.fit(x=batch_memory[:, 0:self.n_features], y=q_target, verbose=0, batch_size=64, epochs=10)
-        self.cost_his.extend(history.history['loss'])
+        history = self.model_eval.fit(x=batch_memory[:, 0:self.n_features], y=q_target, verbose=0, batch_size=32,
+                                      epochs=10)
+        # self.each_epoch_cost_his.extend(history.history['loss'])
+        self.each_step_cost_his.append(np.mean(history.history['loss']))  # 记录每个step的平均loss
 
         self.learn_step_counter += 1
 
@@ -90,13 +86,13 @@ class DQN:
             action = np.squeeze(np.argmax(action_vals, axis=1))
         else:
             action = np.random.randint(0, self.n_actions)
+        # print('action is ' + action)
         return action
 
-    def plot_cost(self):
-        print('end,cost_his size:%d' % len(self.cost_his))
-        plt.clf()
-        # plt.legend()
-        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
-        plt.ylabel('Cost')
-        plt.xlabel('training steps')
-        plt.show()
+    # def plot_cost(self):
+    #     print('end,cost_his size:%d' % len(self.each_epoch_cost_his))
+    #     plt.clf()
+    #     plt.plot(np.arange(len(self.each_epoch_cost_his)), self.each_epoch_cost_his)
+    #     plt.ylabel('Cost')
+    #     plt.xlabel('training steps')  # step代表每个epoch（self.model_eval.fit的入参）
+    #     plt.show()
--- run_MountainCar.py
+++ run_MountainCar.py
@@ -1,19 +1,42 @@
 import gym
-from RL_brain import DQN
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from RL_brain import DQN
+
+
+def plot_reward_and_cost(i_episode, size):
+    if i_episode == 0:
+        plt.figure()
+        plt.ion()
+    elif i_episode >= size - 1:
+        plt.ioff()
+        plt.show()
+        return
+    plt.clf()
+    plt.subplot(2, 1, 1)
+    plt.plot(np.arange(len(step_reward_list)), step_reward_list)
+    plt.title('each_step_reward')
+    plt.subplot(2, 1, 2)
+    plt.plot(np.arange(len(RL.each_step_cost_his)), RL.each_step_cost_his)  # 1个episode训练step次，每次训练遍历样本epoch次
+    plt.title('each_step_cost')
+    plt.pause(0.00000001)
+
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 env = gym.make('MountainCar-v0')
 RL = DQN(3, 2)
-total_steps = 0  # 记录步数
 
 ## 玩100回合，边玩边产生样本，边训练
-reward_list = []
-for i_episode in range(1000):
+step_reward_list = []  # 记录每个step的reward
+episode_cnt = 300
+total_steps = 0  # 记录步数
+
+for i_episode in range(episode_cnt):
     print('episode:%d' % i_episode)
     observation = env.reset()
-    reward_sum = 0
     while True:
         env.render()
         action = RL.choose_action(observation)
@@ -24,34 +47,17 @@ for i_episode in range(1000):
         reward = abs(position - (-0.5))
 
         RL.store_transition(observation, action, reward, observation_)
-        reward_sum += reward
 
         if total_steps > 1000:
+            step_reward_list.append(reward)
             RL.learn()
 
         if done:
-            reward_list.append(reward_sum)
             break
 
         observation = observation_
         total_steps += 1
-    print('reward_list:%s' % reward_list)
+    plot_reward_and_cost(i_episode, episode_cnt)
 
 env = env.unwrapped
-RL.plot_cost()
-
-## 游戏预览
-## 参考文档：https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
-# for i_episode in range(3):
-#     observation = env.reset() ##状态初始化，初始化一个高度，速度置为0
-#     for t in range(1000):
-#         env.render() ## 画面刷新
-#         print(observation)
-#         action = env.action_space.sample()
-#         print(action)
-#         observation, reward, done, info = env.step(action)
-#         print(reward)
-#         if done:
-#             print("Episode finished after {} timesteps".format(t+1))
-#             break
-# env.close()
+# RL.plot_cost()
